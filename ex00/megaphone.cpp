6efbb4e4623b0401eb1a06a607f5a346811c8183
Darkless12 <dftlg@yahoo.com>
2025-10-09 14:16:32 +0100

    ex00 is done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	std::string	word;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		word = argv[i];
+		for (int j = 0; j < word.size(); j++)
+		{
+			std::cout << (char)std::toupper(word[j]);
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
