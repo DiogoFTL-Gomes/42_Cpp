81c766165335cbd8a86dfba4f875858a8ebe00f4
Darkless12 <dftlg@yahoo.com>
2025-10-15 15:01:12 +0100

    Making zombies...

---
+++ Cpp01/ex00/Zombie.cpp
@@ -0,0 +1,7 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(void){
+}
+
+Zombie::~Zombie(void){
+}
--- Cpp01/ex00/Zombie.hpp
+++ Cpp01/ex00/Zombie.hpp
@@ -1,6 +1,18 @@
 #ifndef ZOMBIE_H
 # define ZOMBIE_H
 
+# include <iostream>
 
+class Zombie{
+
+	public:
+		Zombie(void);
+		~Zombie(void);
+
+		void	announc(void) const;
+
+	private:
+		std::string	_name;
+};
 
 #endif
