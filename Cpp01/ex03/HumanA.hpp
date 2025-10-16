89dd556bdeb98fb9f29e37c3c6d5ca42adf512ba
Darkless12 <dftlg@yahoo.com>
2025-10-16 18:16:05 +0100

    This is violent

---
+++ Cpp01/ex03/HumanA.hpp
@@ -0,0 +1,15 @@
+#ifndef HUMANA_H
+# define HUMANA_H
+
+#include <iostream>
+#include "Weapon.hpp"
+
+class HumanA{
+	public:
+		HumanA(std::string name, Weapon weapon_type);
+		~HumanA();
+	private:
+};
+
+
+#endif
