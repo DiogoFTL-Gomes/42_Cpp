8b787175383600882f23c55bc8fdf5ee2f8378ad
Darkless12 <dftlg@yahoo.com>
2025-10-17 15:13:43 +0100

    I/O Operations seem to need more steps and arguments. String operations look powerfull

---
+++ Cpp01/ex04/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int argc, char *argv[]){
+
+	if (argc != 4){
+		std::cerr << "Program needs <file> <string_to_replace> <new_string>" << std::endl;
+		return (1);
+	}
+
+	std::string	sourcefile = argv[1];
+	std::string	s_target = argv[2];
+	std::string	s_new = argv[3];
+	if (sourcefile.length() == 0 || s_target.length() == 0){
+		std::cerr << "Cannot have empty strings" << std::endl;
+	}
+
+	//antigo open(fd) :)
+	std::ifstream	input_file(sourcefile);
+	if (!input_file.is_open()){
+		std::cerr << "Could not open input file " + sourcefile << std::endl;
+		return (1);
+	}
+
+	std::string	content;
+	std::getline(input_file, content, '\0');//delimitador para evitar que pare em \n
+
+	size_t	pos = content.find(s_target);//position where a s_target is found in content starting at 0 by default
+	while (pos != std::string::npos){//npos is value that is usualy returned when a string fails?
+		content = content.substr(0, pos) + s_new + content.substr(pos + s_target.length());//substr(a,b) returns string from a to b.
+		pos = content.find(s_target, pos +s_new.length());
+	}
+
+	std::ofstream	output_file(sourcefile + ".replace");
+	if (!output_file.is_open()){
+		std::cerr << "Could not open output file " + sourcefile + ".replace" << std::endl;
+		return (1);
+	}
+
+	output_file << content;
+	return 0;
+}
