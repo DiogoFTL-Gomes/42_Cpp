657d0d9732619d18c628fd790ba8b17c56892b90
Darkless12 <dftlg@yahoo.com>
2025-10-18 20:49:00 +0100

    My eyes are full of fixed points....

--- Cpp02/ex01/Fixed.hpp
+++ Cpp02/ex01/Fixed.hpp
@@ -1,19 +1,30 @@
 #ifndef FIXED_H
 # define FIXED_H
 
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+#include <ostream>
+
 class	Fixed{
 	public:
 		Fixed();
 		Fixed(const Fixed &other);
+		Fixed(const int val);
+		Fixed(const float val);
 		Fixed &operator=(const Fixed &other);
 		~Fixed();
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 
 	private:
 		int					_fixedPoint;
 		static const int	_fractBit = 8;
 };
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &value);
 
 #endif
