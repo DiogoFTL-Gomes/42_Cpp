792257670f8184c92f825eab82733164b3d454a1
Darkless12 <dftlg@yahoo.com>
2025-10-20 11:08:32 +0100

    Some tests dont work at school

--- Cpp01/ex01/main.cpp
+++ Cpp01/ex01/main.cpp
@@ -3,19 +3,12 @@
 
 int	main(void){
 	Zombie	*horde;
-	Zombie	*meat;
 	int		many;
-	int		lots;
 
 	many = 5;
-	lots = 500000000;//Rigged to fail!!
 	horde = zombieHorde(many, "Zombie n#");
-	meat = zombieHorde(lots, "Meat Shield n#");
 	if (horde){
 		delete[] horde;
-	}
-	if (meat){
-		delete[] meat;
 	}
 	return (0);
 }
