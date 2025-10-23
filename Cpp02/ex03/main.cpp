ab7a1e7b5378a9694b8eeee240ca9d206d631001
Darkless12 <dftlg@yahoo.com>
2025-10-23 10:30:21 +0100

    Adjusting

--- Cpp02/ex03/main.cpp
+++ Cpp02/ex03/main.cpp
@@ -7,6 +7,10 @@ int	main(void){
 	Point	b(5.0f, 0.0f);
 	Point	c(5.0f, 5.0f);
 	Point	point(3.05f, 3.0f);
+	std::cout << "Point ax:" << a.getX() << " ay: " << a.getY() << std::endl;
+	std::cout << "Point bx:" << b.getX() << " by: " << b.getY() << std::endl;
+	std::cout << "Point cx:" << c.getX() << " cy: " << c.getY() << std::endl;
+	std::cout << "Point x:" << point.getX() << " y: " << point.getY() << std::endl;
 	if (bsp(a, b, c, point) == true)
 		std::cout << "It's IN!!!" << std::endl;
 	else
